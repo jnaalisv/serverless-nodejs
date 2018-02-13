08977892eb6ecc117adbe1dd1c9acdc1c39ff4f7
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-02-13 15:26:49 +0200

    configure env variables to lambda

--- handler.js
+++ handler.js
@@ -17,3 +17,18 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.another = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      LAMBDA_ARN: process.env.LAMBDA_ARN,
+      SERVICE: process.env.SERVICE,
+      IMPORTED: process.env.IMPORTED,
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+}
